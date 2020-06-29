d1c577e4efca3ba11ed23b5a77139eff58836924
Wojciech Dywan <wojciech.dywan@interia.pl>
2020-06-30 01:15:45 +0200

    CircleParticle on target position

--- Soul Hero/Assets/Scripts/PlayerMovement.cs
+++ Soul Hero/Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,14 @@ public class PlayerMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
     private bool moving;
-
+    public ParticleSystem particleClick;
     private CharacterAnimations characterAnimations;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         characterAnimations = GetComponent<CharacterAnimations>();
+        particleClick.Stop();
     }
 
     private void Update()
@@ -25,6 +26,10 @@ public class PlayerMovement : MonoBehaviour
             {
                 // if hit.collider.tag == enemy
                 agent.destination = hit.point;
+                Quaternion rot = Quaternion.Euler(90, 0, 0);
+                particleClick.transform.position = new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z);
+                particleClick.transform.rotation = rot;
+                particleClick.Play();
             }
         }
 
