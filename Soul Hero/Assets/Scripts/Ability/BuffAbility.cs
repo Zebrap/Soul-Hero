f7d7bbefbd59de3f6169f2f3417ef3c4bbd6df80
Wojciech Dywan <wojciech.dywan@interia.pl>
2020-07-09 17:18:31 +0200

    BuffAbility 1.0

---
+++ Soul Hero/Assets/Scripts/Ability/BuffAbility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffAbility : Ability
+{
+    private PlayerMovement playerAttack;
+    public float timeBuff;
+
+    protected override void EffectOnTarget()
+    {
+        playerAttack.attack_damage += spellDamage;
+        StartCoroutine(EndBuff());
+    }
+
+    protected override void MoveAblity()
+    {
+
+    }
+
+    protected override void StartPosition(GameObject parent)
+    {
+        this.transform.SetParent(parent.transform);
+        transform.localPosition = Vector3.zero;
+        playerAttack = parent.GetComponent<PlayerMovement>();
+    }
+
+    IEnumerator EndBuff()
+    {
+        yield return new WaitForSeconds(timeBuff);
+        playerAttack.attack_damage -= spellDamage;
+    }
+}
